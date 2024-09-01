8f07d6c02fb20c68ff772a989901f2ab2947a171
yersitoo <germansitt510@gmail.com>
2024-09-01 18:12:41 -0300

    recuperar

--- screens/login.js
+++ screens/login.js
@@ -1,164 +1,129 @@
-import React, { useState } from 'react';
-import { Text, StyleSheet, View, Image, TextInput, TouchableOpacity, Alert, ImageBackground } from 'react-native';
-import appFirebase from '../credenciales';
-import { getAuth, signInWithEmailAndPassword, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import React, {useState} from 'react'
+import {Text, StyleSheet, View, Image, TextInput, TouchableOpacity, Alert, ImageBackground} from 'react-native'
 
-const auth = getAuth(appFirebase);
-const provider = new GoogleAuthProvider();
 
-export default function Login(props) {
-  const [email, setEmail] = useState();
-  const [password, setPassword] = useState();
+import appFirebase from '../credenciales'
+import {getAuth, signInWithEmailAndPassword} from 'firebase/auth'
+const auth = getAuth(appFirebase)
 
-  const logueo = async () => {
-    try {
-      await signInWithEmailAndPassword(auth, email, password);
-      Alert.alert('Iniciando sesión', 'Accediendo');
-      props.navigation.navigate('Home');
-    } catch (error) {
-      console.log(error);
+export default function Login (props){
+
+    //creamos la variable estado
+    const [email, setEmail] = useState()
+    const [password, setPassword] = useState()
+
+    const logueo = async() =>{
+        try{
+            await signInWithEmailAndPassword(auth,email,password)
+            Alert.alert('Iniciando sesion', 'Accediendo')
+            props.navigation.navigate('Home')
+        }catch (error){
+            console.log(error);
+        }
     }
-  };
 
-  const handleGoogleLogin = async () => {
-    try {
-      const result = await signInWithPopup(auth, provider);
-      const user = result.user;
-      Alert.alert('Iniciando sesión', 'Accediendo');
-      props.navigation.navigate('Home');
-    } catch (error) {
-      console.log(error);
-    }
-  };
+    return(
+        <ImageBackground source={require('../assets/fondo_login.jpg')} style={styles.imagen}>
+            <View style={styles.padre}>
 
-  return (
-    <ImageBackground source={require('../assets/fondo_login.jpg')} style={styles.imagen}>
-      <View style={styles.padre}>
-        <Text style={styles.title}>Bienvenido</Text>
+                <Text style={styles.title}>Bienvenido</Text>
 
-        <View style={styles.tarjeta}>
-          <View style={styles.cajatexto}>
-            <TextInput
-              placeholder="Usuario"
-              style={{ paddingHorizontal: 15, color: 'white' }}
-              onChangeText={(text) => setEmail(text)}
-              placeholderTextColor={'#00B2FF'}
-            />
-          </View>
-          <View style={styles.cajatexto}>
-            <TextInput
-              placeholder="Contraseña"
-              style={{ paddingHorizontal: 15, color: 'white' }}
-              secureTextEntry={true}
-              onChangeText={(text) => setPassword(text)}
-              placeholderTextColor={'#00B2FF'}
-            />
-          </View>
-          <View>
-            <TouchableOpacity onPress={() => props.navigation.navigate('Recuperar')}>
-              <Text style={styles.recuperar}>
-                ¿Olvidaste tu contraseña?{' '}
-                <Text style={{ color: '#00B2FF', textDecorationLine: 'underline' }}>Recuperar</Text>
-              </Text>
-            </TouchableOpacity>
-          </View>
+                <View style={styles.tarjeta}> 
+                    <View style={styles.cajatexto}>
+                        <TextInput placeholder='Usuario' style={{paddingHorizontal:15, color: 'white',}} 
+                        onChangeText={(text)=>setEmail(text)}
+                        placeholderTextColor={'#00B2FF'} />
+                    </View>
+                    <View style={styles.cajatexto}>
+                        <TextInput placeholder='Contraseña' style={{paddingHorizontal:15,color: 'white',}} secureTextEntry={true} 
+                        onChangeText={(text)=>setPassword(text)}
+                        placeholderTextColor={'#00B2FF'} />
+                    </View>
+                    <View >
+                        <TouchableOpacity onPress={() => props.navigation.navigate('Recuperar')}>
+                        <Text style={styles.recuperar}> ¿Olvidaste tu contraseña?   <Text style={{color:'#00B2FF', textDecorationLine:'underline' }}>Recuperar</Text> </Text>
+                        </TouchableOpacity>
+                    </View>
 
-          <View style={styles.padreboton}>
-            <TouchableOpacity style={styles.cajaboton} onPress={logueo}>
-              <Text style={styles.textoboton}>Ingresar</Text>
-            </TouchableOpacity>
-          </View>
+                    <View style={styles.padreboton}>
+                        <TouchableOpacity style={styles.cajaboton} onPress={logueo} >
+                            <Text style={styles.textoboton}> Ingresar </Text>
+                        </TouchableOpacity>
 
-          <View style={styles.padreboton}>
-            <TouchableOpacity style={styles.googleLogin} onPress={handleGoogleLogin}>
-              <Text style={styles.textogoogle}>Iniciar sesión con Google</Text>
-            </TouchableOpacity>
-          </View>
-        </View>
-
-        <View>
-          <Text></Text>
-          <Text></Text>
-        </View>
-        <View>
-          <TouchableOpacity onPress={() => props.navigation.navigate('Register')}>
-            <Text style={styles.registrarse}>
-              No tenes cuenta?{' '}
-              <Text style={{ color: '#00B2FF', textDecorationLine: 'underline' }}>Registrate</Text>
-            </Text>
-          </TouchableOpacity>
-        </View>
-      </View>
-    </ImageBackground>
-  );
+                    </View>
+                </View>
+                <View>
+                    <Text></Text>
+                    <Text></Text>
+                </View>
+                <View> 
+                    <TouchableOpacity onPress={() => props.navigation.navigate('Register')}>
+                    <Text style={styles.registrarse}> No tenes cuenta?    <Text style={{color:'#00B2FF', textDecorationLine:'underline' }}>Registrate</Text></Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+        </ImageBackground>
+    );
 }
 
-const styles = StyleSheet.create({
-  padre: {
-    flex: 1,
-    backgroundColor: '#000000',
-    justifyContent: 'center',
-    alignItems: 'center',
-    paddingHorizontal: 20,
-  },
-  imagen: {
-    width: '100%',
-    height: '100%',
-    opacity: 0.85,
-  },
-  title: {
-    fontSize: 60,
-    fontWeight: 'Bold',
-    color: '#ffffff',
-  },
-  tarjeta: {
-    margin: 20,
-    marginBottom: 100,
-    backgroundColor: 'transparent',
-    borderRadius: 20,
-    width: '90%',
-    padding: 20,
-  },
-  cajatexto: {
-    paddingVertical: 20,
-    backgroundColor: '#4D4D4D',
-    borderRadius: 15,
-    marginVertical: 10,
-    height: 52,
-    opacity: 0.5,
-  },
-  recuperar: {
-    marginTop: 20,
-    color: '#ffffff',
-    textAlign: 'center',
-  },
-  padreboton: {
-    alignItems: 'center',
-  },
-  cajaboton: {
-    backgroundColor: '#00B2FF',
-    borderRadius: 15,
-    paddingVertical: 20,
-    width: '100%',
-    marginTop: 85,
-    height: 60,
-  },
-  textoboton: {
-    textAlign: 'center',
-    color: 'black',
-    fontWeight: 'semibold',
-    fontSize: 18,
+const styles = StyleSheet.create ({
+    padre:{
+        flex: 1,
+        backgroundColor: '#000000',
+        justifyContent: 'center',
+        alignItems: 'center',
+        paddingHorizontal: 20,
+    },
+    imagen:{
+        width:'100%',
+        height:'100%',
+        opacity:0.85,
+    },
+    title:{
+        fontSize: 60,
+        fontWeight: 'Bold',
+        color: '#ffffff',
+    },
+    tarjeta:{
+        margin:20,
+        marginBottom:100,
+        backgroundColor:'transparent',
+        borderRadius:20,
+        width:'90%',
+        padding:20,
+    },
+    cajatexto:{
+        paddingVertical:20,
+        backgroundColor:'#4D4D4D',
+        borderRadius:15,
+        marginVertical:10,
+        height:52,
+        opacity: 0.5,
+    },
+    recuperar:{
+        marginTop:20,
+        color: '#ffffff',
+        textAlign: 'center',
+    },
+    padreboton:{
+        alignItems:'center'
+    },
+    cajaboton:{
+        backgroundColor:'#00B2FF',
+        borderRadius:15,
+        paddingVertical:20,
+        width:'100%',
+        marginTop:85,
+        height: 60,
+    },
+    textoboton:{
+        textAlign:'center',
+        color:'black',
+        fontWeight: 'semibold',
+        fontSize: 18,
     },
     registrarse:{
         color: '#ffffff',
+
     },
-    googleLogin: {
-        color: '#ffffff',
-        textAlign: 'center',
-        paddingVertical: 20,
-        backgroundColor: '#4285F4',
-        borderRadius: 15,
-        width: '100%',
-        marginTop: 20,
-      },
 });
--- screens/recuperar.js
+++ screens/recuperar.js
@@ -1,63 +1,87 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet , TextInput, TouchableOpacity, Alert } from 'react-native';
-import getAuth from '@react-native-firebase/auth';
-import firebase from '../credenciales';
+import { View, Text, StyleSheet, TextInput, TouchableOpacity, Alert, ImageBackground} from 'react-native';
+import { getAuth, sendPasswordResetEmail } from 'firebase/auth';
+import appFirebase from '../credenciales';
 
-const auth = getAuth(firebase);
+const auth = getAuth(appFirebase);
 
-export default function Recuperar (props) {
-    const [email, setEmail] = useState('');
+export default function Recuperar(props) {
+  const [email, setEmail] = useState('');
 
-    const handleRecoverPassword = async () => {
-      try {
-        await getAuth(firebase).sendPasswordResetEmail(email);
-        Alert.alert('Correo electrónico enviado', 'Se ha enviado un correo electrónico para recuperar tu contraseña');
-      } catch (error) {
-        console.log(error);
+  const handleRecuperar = async () => {
+    let isError = false;
+    try {
+      await sendPasswordResetEmail(auth, email);
+      Alert.alert('Correo electrónico enviado', 'Se ha enviado un correo electrónico para recuperar tu contraseña');
+    } catch (error) {
+      console.log(error);
+      Alert.alert('Correo invalido','Intentalo de nuevo')
+      isError = true;
+    }if (!isError) {
+        setTimeout(() => {
+          // Código que se ejecutará después del delay
+          props.navigation.navigate('Login')
+        }, 1000);
       }
-    };
-  
-    return (
-      <View style={styles.container}>
-        <Text style={styles.title}>Recuperar contraseña</Text>
-        <TextInput
-          style={styles.input}
-          placeholder="Correo electrónico"
-          value={email}
-          onChangeText={(text) => setEmail(text)}
-        />
-        <TouchableOpacity style={styles.button} onPress={handleRecoverPassword}>
-          <Text style={styles.buttonText}>Enviar correo electrónico</Text>
-        </TouchableOpacity>
-      </View>
-    );
-  }
-  
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
+  };
+
+  return (
+    <ImageBackground source={require('../assets/fondo_registro.jpg')} style={styles.imagen}>
+    <View style={styles.container}>
+      <Text style={styles.title}>Recuperar contraseña</Text>
+      <View style={styles.input}>
+      <TextInput placeholder="Correo electrónico" style={{paddingHorizontal:15,color: 'white',}} placeholderTextColor="#ffffff" value={email} onChangeText={(text) => setEmail(text)}/>
+        </View>
+      <TouchableOpacity style={styles.button} onPress={handleRecuperar}>
+        <Text style={styles.buttonText}>Enviar</Text>
+      </TouchableOpacity>
+    </View>
+    </ImageBackground>
+  );
+}
+
+const styles = StyleSheet.create({
+    imagen:{
+        width: '100%',
+        height: '100%',
+        opacity: 0.80,
     },
-    title: {
-      fontSize: 24,
-      fontWeight: 'bold',
-      marginBottom: 20,
-    },
-    input: {
-      height: 40,
-      borderColor: 'gray',
-      borderWidth: 1,
-      paddingHorizontal: 10,
-      paddingVertical: 10,
-    },
-    button: {
-      backgroundColor: '#007bff',
-      padding: 10,
-      borderRadius: 5,
-    },
-    buttonText: {
-      color: 'white',
-      fontSize: 16,
-    },
-  });
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingHorizontal: 20,
+    backgroundColor: '#000000',
+    padding: 20,
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: 'bold',
+    marginBottom: 20,
+    color: '#ffffff',
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    paddingHorizontal: 10,
+    paddingVertical: 10,
+    marginVertical: '10%',
+    width: '90%',
+    borderRadius: 15,
+    color: '#ffffff',
+  },
+  button: {
+    backgroundColor: '#00B2FF',
+    padding: 10,
+    borderRadius: 15,
+    width: '90%',
+    height: 45,
+    marginTop: '10%',
+  },
+  buttonText: {
+    color: 'black',
+    fontSize: 18,
+    textAlign: 'center',
+  },
+});
