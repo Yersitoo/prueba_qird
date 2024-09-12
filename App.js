01cb059df7c83036680b9da12bd5fa47221beeed
yersitoo <germansitt510@gmail.com>
2024-09-12 16:56:20 -0300

    bottom bar styles

--- App.js
+++ App.js
@@ -1,14 +1,5 @@
+import { StatusBar } from 'expo-status-bar';
 import {StyleSheet, Text, View } from 'react-native';
-<<<<<<< HEAD
-import * as React from 'react';
-import Login from './screens/login'
-
-
-function App(){
-return(
-  <Login/>
-);
-=======
 import 'react-native-gesture-handler';
 import Mavigation from './Mavigation';
 
@@ -17,20 +8,5 @@ export default function App() {
   return (
     <Mavigation/>
   );
->>>>>>> 8979a56ce1998483fb66bb5dc81285e1aa2cba1d
 }
-export default App
 
-
-<<<<<<< HEAD
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
-=======
->>>>>>> 8979a56ce1998483fb66bb5dc81285e1aa2cba1d
--- Mavigation.js
+++ Mavigation.js
@@ -9,17 +9,59 @@ import Login from './screens/login';
 import Register from './screens/register';
 import Recuperar from './screens/recuperar';
 import { NavigationContainer } from '@react-navigation/native';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const Tabs = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
 
+const homeName = 'Home';
+const clasesName = 'Clases'
+const qrName = 'qr'
+const notifName = 'Notificaciones'
+const userName = 'User'
+
 function MyTabs() {
   return (
-    <Tabs.Navigator>
-      <Tabs.Screen name='Home' component={Home} options={{ headerShown: false }} />
-      <Tabs.Screen name='Myclases' component={Clases} options={{ headerShown: false }} />
-      <Tabs.Screen name='Notificaciones' component={Notificaciones} options={{ headerShown: false }} />
-      <Tabs.Screen name='User' component={User} options={{ headerShown: false }} />
+    <Tabs.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          if (route.name === homeName) {
+            iconName = focused ? 'home' : 'home-outline';
+          } else if (route.name === clasesName) {
+            iconName = focused ? 'book' : 'book-outline';
+          } else if (route.name === notifName) {
+            iconName = focused ? 'bell' : 'bell-outline';
+          } else if (route.name === userName) {
+            iconName = focused ? 'account' : 'account-outline';
+          }
+
+          return <MaterialCommunityIcons name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: '#009BDE',
+        tabBarInactiveTintColor: '#ccc',
+        tabBarStyle: {
+          backgroundColor: '#1e1e1e', // Fondo oscuro
+          borderTopWidth: 0,
+          elevation: 0,
+          height: 60,
+          borderWidth: 0.5,
+          borderTopWidth: 0.5,
+          borderColor: '#cccccc',
+          paddingBottom: 5,
+        },
+        tabBarIndicatorStyle: {
+          borderWidth: 1,
+          borderColor: '#ccc', // Separaciones entre los iconos de un color gris claro
+          height: 2,
+        },
+      })}
+    >
+      <Tabs.Screen name={homeName} component={Home} options={{ headerShown: false }} />
+      <Tabs.Screen name={clasesName} component={Clases} options={{ headerShown: false }} />
+      <Tabs.Screen name={notifName} component={Notificaciones} options={{ headerShown: false }} />
+      <Tabs.Screen name={userName} component={User} options={{ headerShown: false }} />
     </Tabs.Navigator>
   );
 }
